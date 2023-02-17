d551ead5c27c8a68cef8a3f23f7ec98a58b8d45f
sosthene <sothenetimi94@gmail.com>
2023-02-17 12:07:45 -0800

    Double two-digit combinations

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - printing two two-digit combinations
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i = 0, j = 0;
+
+	while (i <= 99)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
