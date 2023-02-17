eadaf0253cd28b161140b64b49a3d8d2971ea490
sosthene <sothenetimi94@gmail.com>
2023-02-17 11:44:35 -0800

    Three-digit combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints three combination of unique digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	int j, k;
+
+	while (i <= 7)
+	{
+		j = i + 1;
+		while (j < 8)
+		{
+			k = j + 1;
+			while (k <= 9)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
