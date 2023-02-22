e0f1be435cf04eb5e7fd5f5543a8c487528296d2
sosthene <sothenetimi94@gmail.com>
2023-02-22 09:08:27 -0800

    Determining whether a number is pos of neg

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,21 +1,13 @@
 #include "main.h"
 /**
- * positive_or_negative - Checks whether a number is positive or negative
- * @i: number to be checked
- * Return: 0 (Success)
- */
-void positive_or_negative(int i)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+int main(void)
 {
-	if (i < 0)
-	{
-		printf("%d is negative\n", i);
-	}
-	else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else
-	{
-		printf("%d is positive\n", i);
-	}
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
 }
