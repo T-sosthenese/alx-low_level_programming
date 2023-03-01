4d6ded848769b173aef7c28f90b415f7ed9542e8
sosthene <sothenetimi94@gmail.com>
2023-03-01 09:05:32 -0800

    copying string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy - copies a string
+ * @dest: the destination string
+ * @src: the source string
+ * @n: the maximum number of bytes to copy
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
