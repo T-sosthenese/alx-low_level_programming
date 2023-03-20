f87bab73b8b99a5af6f6e238378e06eb310b077f
sosthene <sothenetimi94@gmail.com>
2023-03-20 05:12:28 -0700

    creating struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,32 @@
 #ifndef DOG_H
 #define DOG_H
 
-#define struct dog {
+/*
+ * File: dog.h
+ * Desc: Header file describing struct dog
+ */
+
+/**
+ * struct dog - A new type describing a dog
+ * @name: the name of the dog
+ * @age: the dog's age
+ * @owner: the dog's owner
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
 
-#endif /*DOG_H*/
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
