c633ea520360eec3b64090fb0fa1a6df83e64597
sosthene <sothenetimi94@gmail.com>
2023-04-03 08:22:20 -0700

    freeing more lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: double pointer to the head of the list to be freed.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp_node;
+
+	if (*head == NULL)
+	{
+		return;
+	}
+	while (*head != NULL)
+	{
+		temp_node = *head;
+		*head = (*head)->next;
+		free(temp_node);
+	}
+}
