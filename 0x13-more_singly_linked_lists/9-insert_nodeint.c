5a5db8ac27b5313287ffd74f5a3ec5412e5bb1a3
sosthene <sothenetimi94@gmail.com>
2023-04-04 00:48:23 -0700

    Inserting new node at a specified position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,46 +1,52 @@
+#include <stdio.h>
 #include "lists.h"
-
 /**
- * insert_nodeint_at_index - inserts a node at a specified index
- * @head: head double pointer
- * @idx: index where a node is to be inserted
- * @n: integer to be added to integer data of the struct
- *
- * Return: pointer to the new_node, NULL if failed
+ * insert_nodeint_at_index - inserts a node at any index
+ * @head: pointer to ther first node
+ * @idx: index to insert node, starts at 0
+ * @n: data to pass to the new node
+ * Return: newnode, null if it fails
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *ptr;
+
 	listint_t *new_node;
 
-	listint_t *current;
-
-	unsigned int i;
+	unsigned int j;
 
 	if (head == NULL)
 	{
 		return (NULL);
 	}
-	if (idx == 0)
-	{
-		return (add_nodeint_end(head, n));
-	}
-	current = *head;
-	for (i = 0; current != NULL && i < idx - 1; i++)
-	{
-		current = current->next;
-	}
-	if (current == NULL)
-	{
-		return (NULL);
-	}
 	new_node = malloc(sizeof(listint_t));
+
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
+
 	new_node->n = n;
-	new_node->next = current->next;
-	current->next = new_node;
+	new_node->next = NULL;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	ptr = *head;
+	for (j = 0; j < idx - 1 && ptr != NULL; j++)
+	{
+		ptr = ptr->next;
+	}
+	if (ptr == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = ptr->next;
+	ptr->next = new_node;
 	return (new_node);
 }
