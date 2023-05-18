a47d2c978b85c26648c032fc6c350dd283b672a5
sosthene <sothenetimi94@gmail.com>
2023-05-18 02:38:27 -0700

    printing elements in a linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,15 +11,15 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 
 	if (h == NULL)
 		return (0);
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		count++;
 		h = h->next;
+		count++;
 	}
 	return (count);
 }
