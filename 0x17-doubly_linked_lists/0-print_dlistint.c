3b33b0c119ff306d41061a637c194cb1ab331d1b
sosthene <sothenetimi94@gmail.com>
2023-05-18 02:15:32 -0700

    printing nodes in dlistint_t

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,12 +11,14 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count = 0;
+	int count = 0;
 
+	if (h == NULL)
+		return (0);
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		++count;
+		count++;
 		h = h->next;
 	}
 	return (count);
