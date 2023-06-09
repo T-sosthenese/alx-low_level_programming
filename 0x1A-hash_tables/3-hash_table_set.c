a5422178b4e7c6f33dc6f536bdcbbd0b83d71ecb
T-sosthenese <sosthenetimi94@gmail.com>
2023-06-09 13:16:03 +0300

    Adding element to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,32 +1,39 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Adds an element to the hash table
- * @ht: hash table
- * @key: The key itself
- * @value: The value associated with a key
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table to be set
+ * @key: the key
+ * @value: The value associated with the key
+ *
  * Return: 1 (success), 0 (otherwise)
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index = 0;
+	hash_node_t *element = NULL, *new_node = NULL;
+
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
-	unsigned long int idx;
-	hash_node_t *new_node;
+	index = key_index((unsigned char *) key, ht->size);
+	element = ht->array[index];
 
-	idx = key_index((const unsigned char *)key, ht->size);
+	if (element && strcmp(key, element->key) == 0)
+	{
+		free(element->value);
+		element->value = strdup(value);
+		return (1);
+	}
 
 	new_node = malloc(sizeof(hash_node_t));
-
 	if (new_node == NULL)
 		return (0);
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = ht->array[idx];
-	ht->array[idx] = new_node;
-
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 	return (1);
 }
